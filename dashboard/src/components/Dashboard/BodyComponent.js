d35f1b3e12ae5ec4ca6009677b8b8cecd4a82f52
AustinP <rockmanvnx6@gmail.com>
2019-03-16 22:02:07 +1100

    update component view

--- dashboard/src/components/Dashboard/BodyComponent.js
+++ dashboard/src/components/Dashboard/BodyComponent.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import DeviceName from './DeviceName';
-
+import BarChartItem from './BarChartItem.js';
 export default class BodyComponent extends Component { 
     render () {
         return(
-            <DeviceName />
+            <div className = "main-dashboard">
+                <DeviceName />
+                <BarChartItem />
+            </div>
         )
     }
 }
