7b88d49dd9f6c3676905f632bd09517e682bea4b
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2019-03-17 03:47:00 +1100

    init leaderboard

---
+++ dashboard/src/components/Dashboard/Leaderboard.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+export default class Leaderboard {
+
+    render () {
+
+        return (
+
+            <div>
+            <p>hi</p>
+            </div>
+        )
+}
+}
+
