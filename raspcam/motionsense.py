addb92c887c7333bcf6299a0fda4628d378b7c77
nugyn <linhduyn@gmail.com>
2019-03-16 10:52:33 +0000

    all files uploaded

--- raspcam/motionsense.py
+++ raspcam/motionsense.py
@@ -1,9 +1,33 @@
+import boto3
+import vlc
 import picamera
 import picamera.array
 import time
 
+
 threshold = 50    # required pixel change
 sensitivity = 100 # amount of pixels changes
+
+client = boto3.client('polly', region_name='ap-southeast-2')
+
+response = client.synthesize_speech(
+    OutputFormat='mp3',
+    Text='yeet',
+    TextType='text',
+    VoiceId='Nicole'
+)
+
+#print(response)
+
+audio_stream = response['AudioStream'].read()
+response['AudioStream'].close()
+
+with open('temp.mp3', 'w') as f:
+    f.write(audio_stream)
+
+vlc.MediaPlayer('temp.mp3').play()
+
+
 
 
 def takeFrame(width, height):
@@ -23,7 +47,7 @@ def scanFrameChange(width, height):
     data1 = takeFrame(width, height) #comparison of two 
     while not frameChng:
         data2 = takeFrame(width, height)
-        diffCount = 0L;
+        diffCount = 0;
         for w in range(0, width):
             for h in range(0, height):
                 diff = abs(int(data1[h][w][1]) - int(data2[h][w][1]))
@@ -38,20 +62,25 @@ def scanFrameChange(width, height):
     return frameChng
 
 def changeDetect():
-    print "Detecting"
+    print ("Detecting")
     while True:
         if scanFrameChange(224, 160):
-            print "Motion detected"
+            print ("Motion detected")
             with picamera.PiCamera() as camera:
                 camera.resolution = (1024, 768)
                 camera.start_preview()
                 # Camera warm-up time
-                time.sleep(2)
+                time.sleep(1)
                 camera.capture('foo.jpg')
+                
+    
+                
+                
+                
           
 
 if __name__ == '__main__':
     try:
         changeDetect()
     finally:
-        print "Exiting Program"
+        print ("Exiting Program")
