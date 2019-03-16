6d9ba9d328e852ff5ee890399d65b5fff55991ec
michaeldao <daoduymichael@gmail.com>
2019-03-17 03:22:16 +1100

    ready up the graphs

--- dashboard/src/components/Dashboard/BodyComponent.js
+++ dashboard/src/components/Dashboard/BodyComponent.js
@@ -7,17 +7,14 @@ import _ from 'lodash';
 
 import { Doughnut, HorizontalBar } from 'react-chartjs-2';
 
-import { Card, Grid } from 'semantic-ui-react'
-
-
+import { Grid } from 'semantic-ui-react'
 
 export default class BodyComponent extends Component {
     render() {
         return (
             <div className="main-dashboard">
 
-
-                <DeviceInfo name={this.props.name} id={this.props.id}/>
+                <DeviceInfo name={this.props.name} id={this.props.id} />
 
                 <Load promise={new IotAPI(this.props.token).getMeasurements()}>
                     {({ result, loading }) => {
@@ -25,9 +22,17 @@ export default class BodyComponent extends Component {
                             return <div>Loading</div>
                         }
                         else {
-                            let types = _.groupBy(result, (entry) => entry.value);
+                            let deviceSpecific = result.filter((entry) => entry.device === this.props.id);
+
+                            let types = _.groupBy(deviceSpecific, (entry) => entry.value);
                             let counts = _.map(types, (value) => value.length);
                             let labels = Object.keys(types);
+
+                            console.log(deviceSpecific)
+
+                            let time = _.groupBy(deviceSpecific, (entry) => entry.time)
+
+                            console.log(time)
 
                             const data = {
                                 labels: labels,
@@ -39,18 +44,16 @@ export default class BodyComponent extends Component {
                             };
 
                             return (
-                                <React.Fragment>
-                                    <Grid>
-                                        <Grid.Row>
-                                            <Grid.Column width={8}>
-                                                <Doughnut data={data} />
-                                            </Grid.Column>
-                                            <Grid.Column width={8}>
-                                                <HorizontalBar data={data} />
-                                            </Grid.Column>
-                                        </Grid.Row>
-                                    </Grid>
-                                </React.Fragment>
+                                <Grid>
+                                    <Grid.Row>
+                                        <Grid.Column width={8}>
+                                            <Doughnut data={data} />
+                                        </Grid.Column>
+                                        <Grid.Column width={8}>
+                                            <HorizontalBar data={data} />
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Grid>
                             );
                         }
                     }}
