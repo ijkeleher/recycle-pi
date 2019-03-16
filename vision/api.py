c3d05239cd8a8adca2bbf920dcc488b9c12f9347
Sam Nolan <samnolan555@gmail.com>
2019-03-17 10:54:02 +1100

    Move the api endpoint for publishing measurements

--- vision/api.py
+++ vision/api.py
@@ -1,15 +1,18 @@
 import requests
-import datetime
+from datetime import datetime
+import uuid
 
-BASE_URL = "http://rpi.hazelfire.net/iot"
+BASE_URL = "https://d284504808e347ccb2779337f23759e2.ap-southeast-2.aws.found.io:9243"
 
 
 class IotAPI:
     def post_measurement(self, deviceId, key, value):
-        response = requests.post(BASE_URL + "/measurements/", json={
+        response = requests.post(BASE_URL + "/measurements/doc/", json={
+            'id': int(uuid.uuid1().int) & (1 << 32) - 1,
             'device': deviceId,
             'key': key,
             'value': value,
-            'time': datetime.datetime.now().isoformat() + "Z"
+            'time': datetime.datetime.now().isoformat() + "Z",
+            'location': "Melbourne, Australia",
         })
         print(response.text)
