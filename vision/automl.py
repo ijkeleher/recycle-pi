b5774f01aae0f9e9a1f687f4d565021e70030d7b
nugyn <linhduyn@gmail.com>
2019-03-16 12:14:58 +0000

    updated automl.py

--- vision/automl.py
+++ vision/automl.py
@@ -82,9 +82,8 @@ def evaluate():
 
     condition1 = confident.name == "trash" and float(confident.score) > 0.4
     condition2 = confident.name != "trash" and float(confident.score) <= 0.5
-    condition3 = confident.name == "organic" and float(confident.score) > 0.4
 
-    if condition1 or condition2 or condition3:
+    if condition1 or condition2:
         print("Item is not recyclable.")
         recyclable = "false"
     else:
