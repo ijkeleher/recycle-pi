c33e1c95e43b2bf3e28376ccf4c8aa0b84b82527
AustinP <rockmanvnx6@gmail.com>
2019-03-16 22:44:05 +1100

    modified selectdevice

--- dashboard/src/components/SelectDevice/index.js
+++ dashboard/src/components/SelectDevice/index.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
-import {Button, Input, Modal } from 'semantic-ui-react';
 import './assets/css/selectDevice.css';
+import IotAPI from "../../api";
 export default class SelectDevice extends Component { 
+    constructor(props) {
+        super(props);
+        this.setState({
+            devices: []
+        })
+    }
+    componentDidMount() {
+        IotAPI.getDevices().then(res => {
+            console.log(res);
+        })
+    }
     render() {
         return(
             <div className = "select-panel">
@@ -10,13 +21,6 @@ export default class SelectDevice extends Component {
                     <div className = "device" value="aaedd1f1-12f9-499b-9c5c-990147dc019a">
                         Device1
                     </div>
-                    <Modal trigger={
-                    <div style={{width: "100%", alignItems: 'center'}}>
-                        <Button style={{backgroundColor: "#33d9b2", color: "#fff", margin: '10px auto', display: 'flex'}}>New Device</Button>
-                    </div>}>
-                        <Modal.Header>Add a new item</Modal.Header>
-                        <Modal.Content></Modal.Content>
-                    </Modal>
                 </div>
             </div>
         )
