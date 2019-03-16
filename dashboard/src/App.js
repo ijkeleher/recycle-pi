e1d31b1d383b18cf0d86d3a9d04033242e3dd706
AustinP <rockmanvnx6@gmail.com>
2019-03-16 18:23:08 +1100

    added form login

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,8 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
-import './App.css';
-
+import 'semantic-ui-css/semantic.min.css'
 class App extends Component {
   render() {
     return (
--- dashboard/src/components/Login/index.js
+++ dashboard/src/components/Login/index.js
@@ -1,7 +1,25 @@
 import React, {Component} from 'react';
-
+import { Button, Checkbox, Form } from 'semantic-ui-react';
+import './assets/css/login.css';
 export default class Login extends Component {
   render() {
-    return <div />;
+    return (
+      <div className = "form-login">
+         <Form>
+            <Form.Field>
+              <label>First Name</label>
+              <input placeholder='First Name' />
+            </Form.Field>
+            <Form.Field>
+              <label>Last Name</label>
+              <input placeholder='Last Name' />
+            </Form.Field>
+            <Form.Field>
+              <Checkbox label='I agree to the Terms and Conditions' />
+            </Form.Field>
+            <Button type='submit'>Submit</Button>
+        </Form>
+      </div>
+    );
   }
 }
