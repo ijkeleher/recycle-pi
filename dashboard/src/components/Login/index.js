18dcb8307832135e221a2f23ad4fd2119568a34a
AustinP <rockmanvnx6@gmail.com>
2019-03-16 19:03:00 +1100

    import IOT

--- dashboard/src/components/Login/index.js
+++ dashboard/src/components/Login/index.js
@@ -1,24 +1,32 @@
 import React, {Component} from 'react';
 import { Button, Checkbox, Form } from 'semantic-ui-react';
 import './assets/css/login.css';
+import IotAPI from "../../api";
+import logo from './assets/images/logo.svg';
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
   render() {
     return (
       <div className = "form-login">
-         <h1>RecyclePI</h1>
+         <h1>RecyclePI <img src={logo} style={{height:20, color: '#218c74'}} /></h1>
          <Form>
             <Form.Field>
-              <label>First Name</label>
-              <input placeholder='First Name' />
+              <label>Username</label>
+              <input placeholder='Username' />
             </Form.Field>
             <Form.Field>
-              <label>Last Name</label>
-              <input placeholder='Last Name' />
+              <label>Password</label>
+              <input placeholder='Password' type='password' />
             </Form.Field>
             <Form.Field>
-              <Checkbox label='I agree to the Terms and Conditions' />
+              <Checkbox label='Remember me' />
             </Form.Field>
-            <Button type='submit'>Submit</Button>
+            <Button type='submit' style={{backgroundColor: '#34ace0', color: '#fff'}}>Login</Button>
         </Form>
       </div>
     );
