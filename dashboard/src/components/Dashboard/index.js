325054b1cbb56e965f8df65f482af937d09e84dc
AustinP <rockmanvnx6@gmail.com>
2019-03-17 06:03:56 +1100

    link to leaderboard

--- dashboard/src/components/Dashboard/index.js
+++ dashboard/src/components/Dashboard/index.js
@@ -6,12 +6,26 @@ import './assets/css/dashboard.css';
 import { MenuComponent } from './MenuComponent';
 import BodyComponent from './BodyComponent';
 export default class Dashboard extends Component {
+  constructor (props) {
+    super(props)
+    this.setState({
+      isLeaderBoard: false
+    })
+
+    viewLeaderBoard = () => {
+      this.setState({
+        isLeaderBoard: true
+      })
+    }
+  }
+
 
   render() {
     return (
       <div style={{width: '100%'}}>
         <MenuComponent name={this.props.name} selectDevice={this.props.selectDevice}/>
-        <BodyComponent name={this.props.name} id={this.props.id} status={this.props.status}/>
+        {(!this.state.isLeaderBoard) ? (<BodyComponent name={this.props.name} id={this.props.id} status={this.props.status}/>) : (<LeaderBoard/>)}
+        {/* <BodyComponent name={this.props.name} id={this.props.id} status={this.props.status}/> */}
       </div>
     );
   }
