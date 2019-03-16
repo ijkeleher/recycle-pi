9160e4c23bbfda66099648026c90091edfe54fcb
AustinP <rockmanvnx6@gmail.com>
2019-03-16 22:08:30 +1100

    add select device view

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
+import SelectDevice from './components/SelectDevice';
+
 import 'semantic-ui-css/semantic.min.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Login>  */}
-          <Dashboard />
-        {/* </Login> */}
+        <Login> 
+          <SelectDevice>
+            {/* <Dashboard /> */}
+          </SelectDevice>
+        </Login>
       </div>
     );
   }
---
+++ dashboard/src/components/SelectDevice/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class SelectDevice extends Component { 
+    render() {
+        return(<div>Select device</div>)
+    }
+}
