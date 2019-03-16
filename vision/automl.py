f9a41cc3291b47c77d63de06ed5fa18d6bdcce57
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2019-03-16 20:49:01 +1100

    modified automl.py

--- vision/automl.py
+++ vision/automl.py
@@ -1,66 +1,64 @@
+from google.cloud import automl_v1beta1 as automl
+
 class Item:
     def __init__(self, name, score):
         self.name = name
         self.score = score
 
-project_id = 'recycle-pi'
-compute_region = 'us-central1'
-model_id = 'ICN1955812788478599087'
-file_path = 'test.jpg'
-score_threshold = '0.0'
+def evaluate():
+    project_id = 'recycle-pi'
+    compute_region = 'us-central1'
+    model_id = 'ICN1955812788478599087'
+    file_path = 'test.jpg'
+    score_threshold = '0.0'
 
-from google.cloud import automl_v1beta1 as automl
+    automl_client = automl.AutoMlClient()
 
-automl_client = automl.AutoMlClient()
+    model_full_id = automl_client.model_path(
+        project_id, compute_region, model_id
+    )
 
-# Get the full path of the model.
-model_full_id = automl_client.model_path(
-    project_id, compute_region, model_id
-)
+    prediction_client = automl.PredictionServiceClient()
 
-# Create client for prediction service.
-prediction_client = automl.PredictionServiceClient()
+    with open(file_path, "rb") as image_file:
+        content = image_file.read()
+    payload = {"image": {"image_bytes": content}}
 
-# Read the image and assign to payload.
-with open(file_path, "rb") as image_file:
-    content = image_file.read()
-payload = {"image": {"image_bytes": content}}
+    params = {}
+    if score_threshold:
+        params = {"score_threshold": score_threshold}
 
-# params is additional domain-specific parameters.
-# score_threshold is used to filter the result
-# Initialize params
-params = {}
-if score_threshold:
-    params = {"score_threshold": score_threshold}
+    response = prediction_client.predict(model_full_id, payload, params)
+    print("Prediction results:")
 
-response = prediction_client.predict(model_full_id, payload, params)
-print("Prediction results:")
+    items = []
 
-items = []
+    for result in response.payload:
+        fixed = "{:.5f}".format(result.classification.score)
+        name = result.display_name
+        score = fixed
+        item = Item(name, score)
+        items.append(item)
 
-for result in response.payload:
-    fixed = "{:.5f}".format(result.classification.score)
-    name = result.display_name
-    score = fixed
-    item = Item(name, score)
-    items.append(item)
+    sorted_items = sorted(items, key=lambda x: x.score)
 
-sorted_items = sorted(items, key=lambda x: x.score)
+    for sorted_item in sorted_items:
+        print("Predicted class name: " + sorted_item.name)
+        print("Predicted class score: " + sorted_item.score)
 
-for sorted_item in sorted_items:
-    print("Predicted class name: " + sorted_item.name)
-    print("Predicted class score: " + sorted_item.score)
+    length = len(sorted_items)
+    confident = sorted_items[length - 1]
 
-length = len(sorted_items)
-confident = sorted_items[length - 1]
+    print("\nMost confident class name: " + confident.name)
+    print("Most confident class score: " + confident.score)
 
-print("\nMost confident class name: " + confident.name)
-print("Most confident class score: " + confident.score)
+    condition1 = confident.name == "trash" and float(confident.score) > 0.4
+    condition2 = confident.name != "trash" and  float(confident.score) <= 0.5
 
-condition1 = confident.name == "trash" and float(confident.score) > 0.4
-condition2 = confident.name != "trash" and  float(confident.score) <= 0.5
+    if condition1 or condition2:
+        print("Item is not recyclable.")
+    else:
+        print("Item is recyclable.")
 
-if condition1 or condition2:
-    print("Item is not recyclable.")
-else:
-    print("Item is recyclable.")
+if __name__ == '__main__':
+   evaluate()
