9aa6575fb7a7dac20116069819da9aea3579efec
nugyn <linhduyn@gmail.com>
2019-03-17 00:46:07 +0000

    updated api key

--- vision/automl.py
+++ vision/automl.py
@@ -101,7 +101,7 @@ def evaluate():
     sleep(5)
     runapi = IotAPI()
     runapi.post_measurement(
-        "1626438031", "wasteType", confident.name)
+        1, "wasteType", confident.name)
     
     print("\nDetecting motion...")
     speek("Detecting motion...")
